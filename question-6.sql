bb5ce827e10720ec596e81b3e6561e95df8735de
nlee777 <nathanlee.aos@gmail.com>
2023-04-11 16:43:24 -0700

    changes

--- question-6.sql
+++ question-6.sql
@@ -1,5 +1,6 @@
 -- Find tracks (id, name and composer) that are part of a line in an invoice.
-
+-- is group by used correctly here?
+-- should i order this in a particular way?
 SELECT tracks.TrackId, tracks.name, tracks.composer 
 FROM tracks 
 JOIN invoice_items ON tracks.TrackId = invoice_items.TrackId 
